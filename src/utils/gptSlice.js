994a1e706a429e59060028777fd2f66304241176
Harshita <joshiharshita43@gmail.com>
2025-10-25 21:31:07 +0530

    fixed keys and api hits

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,14 +4,22 @@ const gptSlice = createSlice({
     name: 'gpt',
     initialState: {
         showGptSearch: false,
+        MovieResults: null,
+        MovieNames: null,
     },
     reducers: {
         toggleGptSearchView: (state, action) => {
             state.showGptSearch = !state.showGptSearch;
-        }
-    }
+        },
+        addGptMovieResult: (state, action) => {
+            const { movieNames, movieResults} = action.payload;
+
+            state.MovieNames = movieNames;
+            state.movieResults = movieResults;
+        },
+    },
 })
 
 
-export const { toggleGptSearchView } = gptSlice.actions;
+export const { toggleGptSearchView, addGptMovieResult} = gptSlice.actions;
 export default gptSlice.reducer;
